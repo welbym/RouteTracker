2de9c5ca15929a1c2ab8aea84835967d633e3341
welbym <welbymoore0816@gmail.com>
2021-03-26 19:43:37 -0500

    Update MapReceiver.java

--- app/src/main/java/com/example/routetracker/MapReceiver.java
+++ app/src/main/java/com/example/routetracker/MapReceiver.java
@@ -3,6 +3,7 @@ package com.example.routetracker;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public interface MapReceiver {
+    void recordLandmark();
     void track(boolean tracking);
     OnMapReadyCallback receiveMapReadyCallback();
 }
